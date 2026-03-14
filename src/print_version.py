fe48d2bf173981910932ee2e1f161b12934943af
mennasalah <menna.skh2003@gmail.com>
2026-03-15 00:18:47 +0200

    fix: changed printer function to a getter function and solved no return issue

--- src/print_version.py
+++ src/print_version.py
@@ -1,15 +1,16 @@
 import sys
 
-def version_printer(version_file):
+def version_getter(version_file):
+    """Retreives the current version tag of the hivebox app (and image)"""
     try:
-        with open(version_file, "r") as file:
+        with open(version_file, "r", encoding="utf-8") as file:
             version = file.readline()
-            print(version)
     except FileNotFoundError:
         print("Version file not found.")
         sys.exit(1)
+    return version
 
 if __name__ == "__main__":
     # Print version and exit
-        version_printer("version.txt")
-        sys.exit(0)
+    print(version_getter("version.txt"))
+    sys.exit(0)
