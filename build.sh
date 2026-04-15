6f565e96bf214f38813edcea30ad5905565951c5
mennasalah <menna.skh2003@gmail.com>
2026-04-15 02:45:38 +0200

    fix: build varaible not redirected any calue false/true from build script

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #! /bin/env bash
 
-VERSION_FILE=src/version.txt
-
-git tag -l --sort=-taggerdate | grep v[0-9].[0-9].[0-9] | head -1 > $VERSION_FILE
+VERSION=$(git tag -l --sort=-taggerdate | grep v[0-9].[0-9].[0-9] | head -1)
 
 # if the latest (HEAD) commit is tagless (in vX.X.X format), the workflow shuts down gracefully
 if [ -z "$VERSION" ]; then
   echo "No tag found, skipping build"
+  echo "built=false" >> $GITHUB_OUTPUT
   exit 0 
 fi
-
-VERSION=$(cat $VERSION_FILE)
+echo "$VERSION" > src/version.txt
 
 docker image build -t hivebox:${VERSION:1} -t hivebox:latest .
+echo "built=true" >> $GITHUB_OUTPUT
