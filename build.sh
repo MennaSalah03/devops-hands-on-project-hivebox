f443cc0299c4df270cd7192f0068f02950f901c2
mennasalah <menna.skh2003@gmail.com>
2026-07-28 18:48:57 +0300

    fixed lower case issue in cd  when using github credentials for docker images

--- build.sh
+++ build.sh
@@ -9,14 +9,6 @@ else
   VERSION=$(git rev-parse --short HEAD)
 fi
 
-# # if the latest (HEAD) commit is tagless (in vX.X.X format), the workflow shuts down gracefully
-# if [[ ! "$VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]];
-# then
-#   echo "No suitable tag found, skipping build"
-#   echo "built=false" >> $GITHUB_OUTPUT
-#   exit 0 
-# fi
-
 echo "$VERSION" > src/version.txt
 
 docker image build -t hivebox:$VERSION .
